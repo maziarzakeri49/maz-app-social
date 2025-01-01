985c2ff27487a27ab5aa649cbf08083e08b7845d
maziarzakeri49 <maziarzakeri49@gmail.com>
2025-01-01 17:57:08 -0500

    Add side bars components

--- src/app/(other)/layout.jsx
+++ src/app/(other)/layout.jsx
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import ".././globals.css";
+import LeftSideBar from "@/components/LeftSideBar";
+import RightSideBar from "@/components/RightSideBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +24,11 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="flex justify-between max-w-6xl mx-auto">
+          <div className="hidden sm:inline border-r h-screen sticky top-0"><LeftSideBar/></div>
+          <div className="w-0 flex-1"> {children}  </div>
+          <div className=" lg:flex-col p-3 h-screen border-l hidden lg:flex w-[24rem]"><RightSideBar/></div>
+        </div>
       </body>
     </html>
   );
